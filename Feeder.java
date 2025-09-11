1b4adfdb39dc6e65166367cafb31c2c52cc22aba
JoshuaAddanki <joshuaa521@nycstudents.net>
2025-09-11 15:35:11 -0400

    Added a feeder

--- Feeder.java
+++ Feeder.java
@@ -1,11 +1,17 @@
 public class Feeder {
-
     /**
      * The amount of food, in grams, currently in the bird feeder; initialized
      * in the constructor and always greater than or equal to zero
      */
     private int currentFood;
-
+    public Feeder(int food)
+    {
+        currentFood = food;
+    }
+    public int getCurrentFood()
+    {
+        return currentFood;
+    }
     /**
      * Simulates one day with numBirds birds or possibly a bear at the bird
      * feeder, as described in part (a) Precondition: numBirds > 0
