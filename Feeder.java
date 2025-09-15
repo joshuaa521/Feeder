e31e745fc09ab90862dd85ac4073eff2c0fe0a36
JoshuaAddanki <joshuaa521@nycstudents.net>
2025-09-15 15:30:41 -0400

    Finished project

--- Feeder.java
+++ Feeder.java
@@ -4,20 +4,27 @@ public class Feeder {
      * in the constructor and always greater than or equal to zero
      */
     private int currentFood;
-    public Feeder(int food)
-    {
+
+    public Feeder(int food) {
         currentFood = food;
     }
-    public int getCurrentFood()
-    {
+
+    public int getCurrentFood() {
         return currentFood;
     }
+
     /**
      * Simulates one day with numBirds birds or possibly a bear at the bird
      * feeder, as described in part (a) Precondition: numBirds > 0
      */
-    public void simulateOneDay(int numBirds)
-    {
+    public void simulateOneDay(int numBirds) {
+        if (Math.random() < 0.05) currentFood = 0;
+        else {
+            int eaten = (int) (Math.random() * 41) + 10;
+            eaten *= numBirds;
+            currentFood -= eaten;
+            if (currentFood < 0) currentFood = 0;
+        }
     }
 
     /**
@@ -25,9 +32,16 @@ public class Feeder {
      * feeder in this simulation, as described in part (b) Preconditions:
      * numBirds > 0, numDays > 0
      */
-    public int simulateManyDays(int numBirds, int numDays) 
-    {
-        return 0;
+    public int simulateManyDays(int numBirds, int numDays) {
+        int days = 0;
+        while (numDays > 0) {
+            if (currentFood == 0) return days;
+            simulateOneDay(numBirds);
+            numDays--;
+            days++;
+
+        }
+        return days;
     }
 
 }
