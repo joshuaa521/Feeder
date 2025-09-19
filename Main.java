4e9e89189d08b983512f3766f9156e7826a7642f
JoshuaAddanki <joshuaa521@nycstudents.net>
2025-09-19 15:01:24 -0400

    Added test cases

--- Main.java
+++ Main.java
@@ -2,10 +2,26 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder(250);
+        Feeder f = new Feeder(500);
+        f.simulateOneDay(12);
+        System.out.println(f.getCurrentFood() + " grams");
+
+        f = new Feeder(1000);
+        f.simulateOneDay(22);
+        System.out.println(f.getCurrentFood() + " grams");
+
+        f = new Feeder(100);
+        f.simulateOneDay(5);
+        System.out.println(f.getCurrentFood() + " grams");
+
+        f = new Feeder(2400);
+        System.out.println(f.simulateManyDays(10, 4) + " grams");
+
+        f = new Feeder(250);
         System.out.println(f.simulateManyDays(10,5) + " day(s)");
         f = new Feeder(0);
         System.out.println(f.simulateManyDays(5,10) + " day(s)");
+
 
     }
 }
